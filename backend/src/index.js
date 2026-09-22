ab7a763dabf9cbed67bc2e841897f7ca80c50a91
Garvit <garvityadav992@gmail.com>
2026-09-22 19:08:18 +0530

    2nd commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
+import User from "./models/user.model.js";
+import { clerkMiddleware } from '@clerk/express'
+import cors from "cors";
+
+
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL ;
 
+
+app.use(express.json());
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true,
+}));
+app.use(clerkMiddleware());
+
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
 
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
