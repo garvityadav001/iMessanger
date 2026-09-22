5bdf641d02c5b513a4a206c13ef4cf90c0b84af3
Garvit <garvityadav992@gmail.com>
2026-09-22 19:33:14 +0530

    4th commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import {connectDB} from "./lib/db.js";
 import User from "./models/user.model.js";
 import { clerkMiddleware } from '@clerk/express'
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 
 dotenv.config();
@@ -12,6 +14,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 const FRONTEND_URL = process.env.FRONTEND_URL ;
+
+const publicDir = path.join(process.cwd(), "public");
 
 
 app.use(express.json());
@@ -26,6 +30,14 @@ app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
+
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
