508a50d7c5e0b16550008a5db564438f7e4dd2e8
Garvit <garvityadav992@gmail.com>
2026-09-23 19:22:02 +0530

    devlop webhooks

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import fs from "fs";
 import path from "path";
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 
 
 dotenv.config();
@@ -17,6 +18,8 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL ;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 
 app.use(express.json());
