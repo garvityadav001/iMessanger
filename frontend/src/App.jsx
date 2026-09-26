6d50228929299490d2648b747d23e5fd0595ca4f
Garvit <garvityadav992@gmail.com>
2026-09-26 12:12:03 +0530

    add authentication component and improve loading experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,12 @@ import { Navigate, Routes,Route } from 'react-router';
 import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import {useAuth} from "@clerk/react"
+import PageLoader from './components/PageLoader';
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
    
-   if(!isLoaded) return <p>Loading...</p>;
+   if(!isLoaded) return <PageLoader />;
    
   return (
     <ThemeProvider>
